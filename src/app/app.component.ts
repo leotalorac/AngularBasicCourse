c9ebdbbc770fba35b7391f1cef97eaa90be5734b
leotalorac <leotalorac@unal.edu.co>
2022-07-27 09:49:58 -0500

    Basic angular template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  name = 'lotalorafox';
+  age = 23;
+  imgurl = "https://source.unsplash.com/random";
+  disable = true;
+  valuetext = 'lios'
+  person = {
+    name: "luis",
+    age: 23,
+    avatar: this.imgurl
+  }
+
+  toggleButton() {
+    this.disable=!this.disable;
+    this.person.age++;
+  }
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+  changeText(event:Event){
+    const element = event.target as HTMLInputElement;
+    this.valuetext=element.value;
+    console.log(this.valuetext)
+  }
 }
