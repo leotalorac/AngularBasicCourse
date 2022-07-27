4298840620415c108f128500325abbc8e0890611
leotalorac <leotalorac@unal.edu.co>
2022-07-27 14:37:47 -0500

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  widthImg = 10;
+  box={
+    w:10,
+    h:12,
+    color:'green'
+  };
   name = 'lotalorafox';
   age = 23;
   imgurl = "https://source.unsplash.com/random";
@@ -17,7 +24,44 @@ export class AppComponent {
     age: 23,
     avatar: this.imgurl
   }
+  color:string = "blue"
 
+  emojis:string[] = [ '😂' , '🐦', '🐳','🌮', '💚']
+  newEmoji = ""
+
+  products:Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
   toggleButton() {
     this.disable=!this.disable;
     this.person.age++;
@@ -30,5 +74,12 @@ export class AppComponent {
     const element = event.target as HTMLInputElement;
     this.valuetext=element.value;
     console.log(this.valuetext)
+  }
+  AddEmoji(){
+    this.emojis.push(this.newEmoji);
+    this.newEmoji=""
+  }
+  deleteEmoji(index:number){
+    this.emojis.splice(index,1)
   }
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  name:string;
+  image:string;
+  price:number;
+  category?:string;
+}
